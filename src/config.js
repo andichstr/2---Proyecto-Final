7c4facc37a464b907c8d989360dd08085f3c6e39
andichstr <andi.schus@live.com.ar>
2023-01-10 00:32:57 -0300

    Config firebase

--- src/config.js
+++ src/config.js
@@ -11,18 +11,19 @@ export default {
             // serverSelectionTimeoutMS: 5000,
         }
     },
-    firebase: { //Cambiar por la config personal
+    firebase: {
         "type": "service_account",
-        "project_id": "probando-d0b5e",
-        "private_key_id": "a40c76091183a33f095228f7c8b00e4f474c74f5",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCi2plOAtUbKBcz\n28E4E+8voT7KcYsx/8VkTbLds25emgrOkA8oCuBo98QZvHn4DP7NpyDiLCIdXosO\nTJ9AfMzT3sQLZHII8Yfa20PozLKbGPWeGaT2WBeo6SkKEdshqTiSM+t9FbprpMPN\nRVLfDTDNziC6L6zzH2W8zmD3hBKmp+SkIldsHkd4G5Rnfzf89bWflUkaaODcicMt\nMN3PGyCnE//88feV+HOqALfA6XGviyyBQSvllnIIXOGGcFlnhP1ek/MoxPPd8rrU\nGy84Dvjat5hKcelu7V2I2pnauwDz0NuWUZBhbvNS/0y6eg1dkAq0QAoIqTBPBo2Q\n/H74aq93AgMBAAECggEAGKPypjcPf3hlaldJuO5rNk2zbnYXlY0ZlL+c3KaVgILW\nN8LcSDa9FaTHaw11du6gbwVmmFINJ1xytcsqHVvEshn2l7Ku/mNtJtQeRk8Shx0T\nrMQ8jXluuwPsTbW97i3ofVaVvutWtYrMhXaDv3QDemBB22nzRSXvAs5HJaw6FjT7\nFmXhmuku2kyn12iysLeE68wKOhqAhHjtMHoxxlx4JRcRo9htlmfVw5L0LTvtJ9nS\nOpO23uPRYihrF32bwJ9iIFl4CUhNYDVwNwUTrFmckNiWfMyAnzeje37QemBb2snA\nZGPNFBmsj0med/4rolDC7aoxp5yVD5qA96ocq5XQgQKBgQDbDgysBN6pkI9O7bo0\nP5sCH5yp07LU9j/JEJUw2JEZEMFpfNF+W3FZlpy4Hkei/jG59qLho6uVoRp1McVY\n+i9xvvNn+z5Qs4NQs9dtPpfzTsjuDLRkCtABL+bpEOCypf0E/bPDcI4dXr4OtJnu\nDkgJTWmnzN73I086NY0nVrj4UQKBgQC+UgGitVmYPPZ6cz/zslnVLsc1RLrvhwoC\n9N21aqg46KWlz+Yxl1QVVzgDWRpcsPZ2qXHzchLr8huP9dmytl3WzyP+QACHEFbT\ncm6lvuENvptYauTuuymUca1DS1QalsvN+Dyw9T9pZSxWP8rBbVJnQ5ghiqDmI5G4\ntbbl4+2BRwKBgGo5MePoqKDJPPUlSnozrdMlyS6BPeMPVncF15LMMfBZs7CXmm8P\noLO02WJfLJXZdfubofiRCY+Rg+b4r54qE/+wfRAbQWiY5ECqNPkAtWaC+eSVvJQW\nui8ZL3wtdwiqfTuCWz2WwxNy8e7nnQ1XV49LPsTkD+r/8FdlyIEfCKtBAoGBALfm\nScpM7bva/Fep0VvLNF54Cul/9hnJJHVi4nCQFOiYdKdZ0haNISRV2LvzdzOlkyFp\n7b/MtFfk45kheC/lY5tTIHZmpZvcv+klI6FoZdKAIV84OuorU6wqg78IpMXkfB4U\nBaD/+TwAFO1qSqLI3d0GRZPzfjGioQYYIBjXOwG1AoGAP7ckkwFdwbMjB0JVp+uq\nJe4UgGwGp1nzX9Gaolm0Fseoz9/dLkmjRncvu08B1MnAfY7z+u0eBS4QVqn4vQyP\nL/Z4xWjWV2nR09N8+ij5snviJOPlyRwXF4pwMjjO4ihLLD9EVnY/+xVlJgcFp1mq\nQIbJvMtCBqv7rI+EYWYllpo=\n-----END PRIVATE KEY-----\n",
-        "client_email": "firebase-adminsdk-bx8rj@probando-d0b5e.iam.gserviceaccount.com",
-        "client_id": "102649946009022189922",
+        "project_id": "coderhouse-31819",
+        "private_key_id": "6ba7335dc5a2f13558e9a0fc987f29aa6aac37a7",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDdQ9ZIxYEZR4Rj\n+j8adKtrrQu8VI/4IbIiifz5pqnv0kizV0ypc4WJeT1U2L4y1zQDkYF5Yotnjto3\nOmnCyafvTh0J2KnjTjP4q153CCdzZaCHZ916sK4OyJYq5lT+q7APJSgwmkB/M3nG\nx76pg2P6Mq6lQEOQrToD315T3BpU3USL1lRNX0erj+1HnuyirP0EwJRQ2rUeN4ez\ncapKgAs2MckRgNYj5fRK0jKCrXb9Zgg5+PLw+56kYe++n/cclChDlAGy9MYKZWG0\nathPJkt7hytM+XBay5auxmAyqr06AuBkmAkkWuiEahXvLf7rS0l1/JoO9PKfTnmm\nE5Zj2TSXAgMBAAECggEAOC+7wGr253ZztfecApnR1dvxePd4P6g+1ep4ZOTfjli1\nwWTPwM/m/wvceSk40yw2txlB6ZMOl1N9Mj0REjem/2u0kAWGzoPTSrgpmsxlSDZs\nVzYW1cr7yCFDkM1AlBMA+DeVRxJEgoiNyd57GgyVXLl2A9yXy6tRah5Uk7L2tMmq\nblg9F00ji9MxkiVsFJ10lEAhnfSMx2+vP7LEDgbgkU8d7atMzevt8XUiXewMGZUk\n4r7bk9D2Rxhrj5HnsdtdZGfP89YUbMpdH0kbOHUjv93xoLuGCxjvt2AcL0VFeBlw\nt3NqvcdvjfKgkLhsKOOFZ7CLwaauL1n99SLuxDKsoQKBgQD0PMZiUoPELFv9bRl9\n/4kg3YxmpvYS3ORMu4VMQvEnWoPjDQIalRcdIZiAA6ShbwEWSCwaq57uaDF/+7uM\n4oLDa6k1MHkYwCiteR8AjBeFjEUsiA+DE2GMHMGOohAUwZWDgzj4snBitkqAWxxz\noaTlLtdKS2jj5QwVFyolb2vHGQKBgQDn69VItwv00oEq7v7r+bivbLE2J1JVGG47\nYL3C5cNlzxH9kmXGRraJrA37bNCgD4dlMaSBmBUQl4V2YEQjSQDyJy5yX0GOirWl\nCfHgaeymqU07fW1/xl5WfrNjlm1cHrtPZC8wqzrE+e5CXh8RQF0ai4nM4l4QydLw\nncnodiS/LwKBgHI8HtoMGB9gVCJjrgG1zLC3jxzQSNYnxQxyxPUXdm3es0M2aV5/\nj1ELzqVJju4G2m5OkXpj/TErQMPIvT83DsxShdEf+/PspNoGhc6et726Dvy2IvM+\n1GhRZuLMwFLE/KaHcNU5uMEi3BPPCa6300VytDXGsaeuEQmoTQk3ZSS5AoGAT0o7\nox6hwdPuQUVwdjDsSV15wK/04A74MOjEN8vh3/7Jgc35aa5nXe4q4Zi0QuTMP3aX\nbY8XNJL5GB2VbhLZdfCbWpLikN8vdvtNbJIKTHh5ZStesl6ZhizPG+8j+GixVVVF\nJPiFoS5pnNsc+ALvUdH/DMMfsdGugoEPtKAftBUCgYAnOuwGWwkk1PZTnqshzVyv\nq0q+hpm/Rns+fhUF8vF1PEjPbEJ/sP2aRKq+6tEVKXm+MJHG4CVq2kFHTGYsbI6I\nxh8+ZoZuXa7+UHVjALOo6sxT+ozIUhMTu8/7adG1KWLAJ03qKTipZ9oFzRJlF7Xh\nGrHo2Zn0VB+gO2MP8nIO6Q==\n-----END PRIVATE KEY-----\n",
+        "client_email": "firebase-adminsdk-hwrr4@coderhouse-31819.iam.gserviceaccount.com",
+        "client_id": "103505461188033111544",
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-bx8rj%40probando-d0b5e.iam.gserviceaccount.com"
-    },
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-hwrr4%40coderhouse-31819.iam.gserviceaccount.com"
+      }
+      ,
     sqlite3: {
         client: 'sqlite3',
         connection: {
--- src/contenedores/ContenedorFirebase.js
+++ src/contenedores/ContenedorFirebase.js
@@ -8,36 +8,59 @@ admin.initializeApp({
 const db = admin.firestore();
 
 class ContenedorFirebase {
-
     constructor(nombreColeccion) {
         this.coleccion = db.collection(nombreColeccion)
     }
-
     async listar(id) {
-
+        try{
+            const doc = this.collection.doc(`${id}`);
+            const item = await doc.get();
+            const response = item.data();
+            return response;
+        }catch(err){
+            console.error(err);
+        }
     }
-
     async listarAll() {
-
+        try{
+            const querySnapshot = await this.coleccion.get();
+            return querySnapshot.docs;
+        }catch(err){
+            console.error(err);
+        }
     }
-
     async guardar(nuevoElem) {
-
+        try{
+            const doc = this.collection.doc();
+            await doc.create(nuevoElem);
+        }catch(err){
+            console.error(err);
+        }
     }
-
     async actualizar(nuevoElem) {
-
+        try{
+            let id = nuevoElem.id;
+            const doc = this.collection.doc(`${id}`);
+            let item = await doc.update(nuevoElem);
+            console.log("Elemento actualizado satisfactoriamente ", item);
+        }catch(err){
+            console.error(err);
+        } 
     }
-
     async borrar(id) {
-
+        try{
+            const doc = this.collection.doc(`${id}`);
+            const item = await doc.delete();
+            console.log("Elemento eliminado satisfactoriamente ", item);
+        }catch(err){
+            console.error(err);
+        }
+        
     }
-
     async borrarAll() {
-
-    }
-
-    async desconectar() {
+        const doc = this.collection.doc();
+        const items = await doc.delete();
+        console.log("Se han borrado todos los elementos de la BD", items)
     }
 }
 
